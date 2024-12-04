e777239594603ebccb73fd63a0e6c895a828c500
AnhDat0103 <nguyentrongdatc2402@gmail.com>
2024-12-04 16:52:13 +0700

    fix to commit with error

--- src/main/java/vn/hoidanit/laptopshop/controller/ProductController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/ProductController.java
@@ -3,10 +3,6 @@ package vn.hoidanit.laptopshop.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.ModelAttribute;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestParam;
-// import org.springframework.web.multipart.MultipartFile;
 
 import vn.hoidanit.laptopshop.domain.Product;
 
@@ -23,12 +19,5 @@ public class ProductController {
         model.addAttribute("newProduct", new Product());
         return "admin/product/create";
     }
-
-    // @PostMapping("/admin/product/create")
-    // public String createProduct(@ModelAttribute("newProduct") Product product,
-    // @RequestParam("datntFile") MultipartFile avatarFile
-    // ){
-    // return "redirect:/admin/product";
-    // }
 
 }
