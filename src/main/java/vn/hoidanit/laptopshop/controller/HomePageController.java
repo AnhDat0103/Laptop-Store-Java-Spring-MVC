d492402afd3a31380c1a016ad1ffac010d7631b0
AnhDat0103 <nguyentrongdatc2402@gmail.com>
2024-12-06 15:50:04 +0700

    feature: load info of product into home page. End show them into detail product for client

--- src/main/java/vn/hoidanit/laptopshop/controller/HomePageController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/HomePageController.java
@@ -1,13 +1,39 @@
 package vn.hoidanit.laptopshop.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
+
+import jakarta.websocket.server.PathParam;
+import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.service.ProductService;
 
 @Controller
 public class HomePageController {
 
-    @GetMapping("")
-    public String showHomePage() {
+    private final ProductService productService;
+
+    public HomePageController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/")
+    public String showHomePage(Model model) {
+        List<Product> list = this.productService.handleShowAllProduct();
+        model.addAttribute("products", list);
         return "client/homepage";
     }
+
+    @GetMapping("/client/product/{id}")
+    public String showDetailProductClient(Model model, @PathVariable long id) {
+        Product product = this.productService.handleFindById(id);
+        model.addAttribute("product", product);
+        return "client/product/detail";
+    }
+
 }
