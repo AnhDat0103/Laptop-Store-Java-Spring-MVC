bf4b9453ae085642f8eccc2fbca266098c9b2cb6
AnhDat0103 <nguyentrongdatc2402@gmail.com>
2024-12-08 20:20:35 +0700

    custom login succes handler such as with admin role -> admin page and with user role -> home page

--- src/main/java/vn/hoidanit/laptopshop/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/laptopshop/config/SecurityConfiguration.java
@@ -10,8 +10,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import jakarta.servlet.DispatcherType;
+import vn.hoidanit.laptopshop.service.MySimpleUrlAuthenticationSuccessHandler;
 import vn.hoidanit.laptopshop.service.UserService;
 import vn.hoidanit.laptopshop.service.customUserDetails.CustomUserDetailsService;
 
@@ -19,48 +21,54 @@ import vn.hoidanit.laptopshop.service.customUserDetails.CustomUserDetailsService
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfiguration {
 
-    private final String[] PUBLIC_ENDPOINT = { "/", "/client/**", "/register", "/client/**", "/css/**", "/js/**",
-            "/images/**" };
+        private final String[] PUBLIC_ENDPOINT = { "/", "/client/**", "/register", "/client/**", "/css/**", "/js/**",
+                        "/images/**" };
 
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+        @Bean
+        PasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
 
-    @Bean
-    UserDetailsService userDetailsService(UserService userService) {
-        return new CustomUserDetailsService(userService);
-    }
+        @Bean
+        UserDetailsService userDetailsService(UserService userService) {
+                return new CustomUserDetailsService(userService);
+        }
 
-    @Bean
-    DaoAuthenticationProvider authenticationProvider(PasswordEncoder passwordEncoder,
-            UserDetailsService userDetailsService) {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailsService);
-        authenticationProvider.setPasswordEncoder(passwordEncoder);
-        authenticationProvider.setHideUserNotFoundExceptions(false);
-        return authenticationProvider;
-    }
+        @Bean
+        DaoAuthenticationProvider authenticationProvider(PasswordEncoder passwordEncoder,
+                        UserDetailsService userDetailsService) {
+                DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+                authenticationProvider.setUserDetailsService(userDetailsService);
+                authenticationProvider.setPasswordEncoder(passwordEncoder);
+                authenticationProvider.setHideUserNotFoundExceptions(false);
+                return authenticationProvider;
+        }
 
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .authorizeHttpRequests(request -> request
-                        .dispatcherTypeMatchers(DispatcherType.FORWARD,
-                                DispatcherType.INCLUDE)
-                        .permitAll()
-                        .requestMatchers(PUBLIC_ENDPOINT).permitAll()
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .anyRequest().authenticated()
+        @Bean
+        public AuthenticationSuccessHandler myAuthenticationSuccessHandler() {
+                return new MySimpleUrlAuthenticationSuccessHandler();
+        }
 
-                )
-                .formLogin(formLogin -> formLogin
-                        .loginPage("/login")
-                        .failureUrl("/login?error")
-                        .permitAll()
+        @Bean
+        SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+                httpSecurity
+                                .authorizeHttpRequests(request -> request
+                                                .dispatcherTypeMatchers(DispatcherType.FORWARD,
+                                                                DispatcherType.INCLUDE)
+                                                .permitAll()
+                                                .requestMatchers(PUBLIC_ENDPOINT).permitAll()
+                                                .requestMatchers("/admin/**").hasRole("ADMIN")
+                                                .anyRequest().authenticated()
 
-                );
-        return httpSecurity.build();
-    }
+                                )
+                                .formLogin(formLogin -> formLogin
+                                                .loginPage("/login")
+                                                .successHandler(myAuthenticationSuccessHandler())
+                                                .failureUrl("/login?error")
+                                                .permitAll()
+
+                                );
+                return httpSecurity.build();
+        }
 
 }
